31e2694ce0ff2cd7f04f001e2270a34f9d8e73f7
arul <rizia.arul@gmail.com>
2021-05-10 18:32:30 +0530

    Create Account

--- Source/app_api/login/views.py
+++ Source/app_api/login/views.py
@@ -47,3 +47,28 @@ def get_sql_column_index(column_name):
                "password": 2
                }
     return mapping[column_name]
+
+
+def create_accounts(request):
+    create_account_returns = {"status": 0, "message": "", "params": {}}
+    if request.method == "POST":
+        try:
+            db = db_connection()
+            firstname = request.POST.get('firstname')
+            lastname = request.POST.get('lastname')
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            print(firstname)
+            # mix_query = """sp_GetGenealogyReportDetails '{curing_lot}'""".format(curing_lot=curing_lot)
+            # check_user_query = """select * from users where user_id = '{uid}' and password = '{pwd}'
+            #             """.format(uid=username, pwd=password)
+            check_user_query = """usp_create_account '{firstName}','{lastName}','{email}','{password}'
+                                """.format(firstName=firstname, lastName=lastname, email=email, password=password)
+            db.execute(check_user_query)
+            check_user = db.fetchall()
+            print(check_user)
+            create_account_returns = check_user
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            print(str(exc_tb.tb_lineno), str(e))
+    return HttpResponse(json.dumps(create_account_returns))
