a4aeb30e49a578888d11c2c114e0f5fd4ba00986
arul <rizia.arul@gmail.com>
2021-05-18 10:46:34 +0530

    settings.py add os reference

--- Source/app_api/errorlog/error_log.py
+++ Source/app_api/errorlog/error_log.py
@@ -5,20 +5,19 @@ import sys
 import os
 
 
-
-
-def developerLog(modulename, method_name,error):
-    directory = settings.CONFIG_PATH+'\\API Developer Log\\'+modulename
-    fileName=""
+def developerLog(modulename, method_name, error):
+    directory = settings.CONFIG_PATH + '\\API Developer Log\\' + modulename
+    fileName = ""
     # Check above directory exists or not
     if not os.path.exists(directory):
         os.makedirs(directory)  # Create a directory if not exists
-    if not os.path.exists(os.path.join(directory, "APILog_"+str(datetime.date.today())+"_"+".txt")):
-        fileName = os.path.join(directory, "APILog_"+str(datetime.date.today())+"_"+".txt")  # Text file name initialize
+    if not os.path.exists(os.path.join(directory, "APILog_" + str(datetime.date.today()) + "_" + ".txt")):
+        fileName = os.path.join(directory,
+                                "APILog_" + str(datetime.date.today()) + "_" + ".txt")  # Text file name initialize
         file1 = open(fileName, "w")  # a means append error in already existsing file
-        file1.write("*****"+str(datetime.datetime.now())+"*****"+'\n'
-                    +method_name+":"+error+'\n'
-                    "********************End**********************")
+        file1.write("*****" + str(datetime.datetime.now()) + "*****" + '\n'
+                    + method_name + ":" + error + '\n'
+                                                  "********************End**********************")
         file1.close()
     else:
         fileName = os.path.join(directory,
@@ -26,5 +25,5 @@ def developerLog(modulename, method_name,error):
         file1 = open(fileName, "a")  # a means append error in already existsing file
         file1.write("*****" + str(datetime.datetime.now()) + "*****" + '\n'
                     + method_name + ":" + error + '\n'
-                    "*********************End*********************")
+                                                  "*********************End*********************")
         file1.close()
