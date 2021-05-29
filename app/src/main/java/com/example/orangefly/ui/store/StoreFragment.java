0e5f06062f7f08da36d00db67d5b7030a20415f4
Abdul <abdulrahmantop@gmail.com>
2021-05-29 12:52:36 +0530

    shown slider view in home page

--- app/src/main/java/com/example/orangefly/ui/store/StoreFragment.java
+++ app/src/main/java/com/example/orangefly/ui/store/StoreFragment.java
@@ -42,17 +42,30 @@ public class StoreFragment extends Fragment {
         final TextView textView = root.findViewById(R.id.text_store);
         textView.setText("All Categories");
         // initializing the slider view.
-        SliderView sliderView = root.findViewById(R.id.slider);
+            SliderView sliderView = root.findViewById(R.id.slider);
         // we are creating array list for storing our image urls.
         ArrayList<SliderBanner> sliderDataArrayList = new ArrayList<>();
         sliderDataArrayList.add(new SliderBanner("url",R.drawable.ofly_banner));
         // passing this array list inside our adapter class.
         SliderAdapter slider_adapter = new SliderAdapter(root.getContext(), sliderDataArrayList);
+        // below method is used to set auto cycle direction in left to
+        // right direction you can change according to requirement.
+        sliderView.setAutoCycleDirection(SliderView.LAYOUT_DIRECTION_LTR);
 
         androidGridView=(GridView)root.findViewById(R.id.store_grid);
         androidGridView.setAdapter(new CustomGridViewActivity(root.getContext(),gridViewString,gridViewImageId));
         // setadapter to sliderview.
         sliderView.setSliderAdapter(slider_adapter);
+        // below method is use to set
+        // scroll time in seconds.
+        sliderView.setScrollTimeInSec(3);
+        // to set it scrollable automatically
+        // we use below method.
+        sliderView.setAutoCycle(true);
+        // to start autocycle below method is used.
+        sliderView.startAutoCycle();
+
+        sliderView.getPagerIndicator().setVisibility(View.VISIBLE);
 
 
 //        storeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
